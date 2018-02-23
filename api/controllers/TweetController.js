55ba6365c525c37fae640c8a5f498c7656b355b6
BeeJayEtzel <brianetzel@gmail.com>
2018-02-23 13:54:41 -0500

    Add functionality to post a tweet

--- api/controllers/TweetController.js
+++ api/controllers/TweetController.js
@@ -6,6 +6,28 @@
  */
 
 module.exports = {
+    /**
+     * addTweet
+     * 
+     * @description :: Adds a tweet from a user to Database
+     */
+    addTweet: function(req,res){
+        var tweet = req.body.message;
+        var usr = req.session.userId;
+        
+        Tweet.create.({
+            post: tweet,
+            user: usr
+        }).exec(function(err) {
+            if (err) {
+                return res.view('500');
+            }
+            else {
+                return res.redirect('/home/' + req.session.username);
+            }
+        });
+    },
+
 	
 };
 
